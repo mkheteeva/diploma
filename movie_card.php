97652699fd8676594d8059ace0a5bfd48621b575
mkheteeva <62591752+mkheteeva@users.noreply.github.com>
2020-05-16 01:36:19 +0300

    Add files via upload

---
+++ movie_card.php
@@ -0,0 +1,31 @@
+﻿<!Doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>MovieCamp</title>
+	<link rel="stylesheet" type="text/css" href="kab_style.css" >
+	<link rel="shortcut icon" href="little_logo.png" type="image/x-icon">
+	<script src="https://kit.fontawesome.com/e4d26e9cd6.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" href="https://bootstraptema.ru/plugins/2015/bootstrap3/bootstrap.min.css" />
+<link rel="stylesheet" href="https://bootstraptema.ru/plugins/font-awesome/4-4-0/font-awesome.min.css" />
+<script src="https://bootstraptema.ru/plugins/jquery/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="https://bootstraptema.ru/plugins/2015/b-v3-3-6/bootstrap.min.js"></script>
+<script src="https://bootstraptema.ru/_sf/3/394.js" type="text/javascript"></script>
+  </head>
+  <header>
+  <div class="topnav">
+  <logot><a  href="kabinet.html"><img src = "little_logo.png" alt = "На главную" width = "75" height = "60"/></a></logot>
+  <a href="messages.html">Мои сообщения</a>
+  <a href="collections.html">Мои коллекции</a>
+
+   <logotwo> <a><img src = "user.png" alt = "Пользователь" width = "35" height = "35"/></a></logotwo>
+   <search> <a href = "find.html"> Поиск </a> </search>
+   <life> <a> Life is better with a good movie </a> </life>
+   </header>
+   <body>
+   <div class = "form-wrap">
+   <img src="joker.jpg" width = "300" height = "420" class= "mov_im"/> 
+   <inf> Джокер </inf>
+   </div>
+   </body>
